22f6d8516303cbb599ce2ba6377985b306e3fb13
KD963 <kalyani24deobhankar@gmail.com>
2019-09-17 19:08:44 +0530

    done

--- ChatApp/FrontEnd/controller/loginController.js
+++ ChatApp/FrontEnd/controller/loginController.js
@@ -1,9 +1,17 @@
 chatApp.controller('loginCtrl',function($scope,loginService){
+
+    this.emailValidation=function(email)
+    {
+        alert(email);
+    }
+     
     $scope.login=function(){
         let loginData={
             'email':$scope.email,
             'password':$scope.password
         }
         loginService.loginServicesUser(loginData,$scope);
+       
     }    
+    $scope.loginName= localStorage.getItem('firstname')
 })
